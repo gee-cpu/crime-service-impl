1a7f165b545dc0abb9e5a322ef207dae8e892e33
gonchaba <ggonchaba@gmail.com>
2023-12-13 16:58:40 +0300

    refactoring the crime-service

--- crime-service/src/main/java/com/gonchaba/crimeservice/CrimeServiceApplication.java
+++ crime-service/src/main/java/com/gonchaba/crimeservice/CrimeServiceApplication.java
@@ -3,6 +3,13 @@ package com.gonchaba.crimeservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,6 +17,20 @@ public class CrimeServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CrimeServiceApplication.class, args);
+    }
+    @Bean
+    public OAuth2AuthorizedClientManager clientManager(ClientRegistrationRepository clientRegistrationRepository,
+                                                       OAuth2AuthorizedClientRepository auth2AuthorizedClientRepository) {
+        OAuth2AuthorizedClientProvider auth2AuthorizedClientProvider
+                = OAuth2AuthorizedClientProviderBuilder.builder()
+                .clientCredentials()
+                .build();
+        DefaultOAuth2AuthorizedClientManager defaultOAuth2AuthorizedClientManager
+                = new DefaultOAuth2AuthorizedClientManager(clientRegistrationRepository,
+                auth2AuthorizedClientRepository);
+        defaultOAuth2AuthorizedClientManager.setAuthorizedClientProvider(auth2AuthorizedClientProvider);
+
+        return clientManager(clientRegistrationRepository, auth2AuthorizedClientRepository);
     }
 
 }
---
+++ crime-service/src/main/java/com/gonchaba/crimeservice/security/WebSecurityConfig.java
@@ -0,0 +1,36 @@
+package com.gonchaba.crimeservice.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig {
+
+    @Bean
+    public DefaultSecurityFilterChain securityWebFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity
+                .authorizeRequests(
+                        authorizeRequest -> {
+                            try {
+                                authorizeRequest
+                                        .anyRequest()
+                                        .authenticated()
+                                        .and().oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+                            } catch (Exception e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+                );
+       return httpSecurity.build();
+    }
+}
